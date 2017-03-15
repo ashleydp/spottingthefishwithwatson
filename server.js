0d3ae80453dd3a6346502c3c6d3abddc77c1962c
Torben Barlow-Hansen <tbarlow@us.ibm.com>
2017-03-15 16:20:20 -0400

    Testing Watson Discovery service

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ function getNews(topic) {
 
 slapp.command('/playlist', (msg) => {
   var message = msg.body.text;
-  // msg.respond(message);
+  msg.respond(message);
 
   if (message == 'dj cat') {
     msg.respond({

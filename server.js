195ac4a2e96673d273800e4f2491f91d3fa1b0b7
Torben Barlow-Hansen <tbarlow@us.ibm.com>
2017-03-15 20:19:17 -0400

    Testing Watson Discovery service

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ slapp.command('/playlist', (msg) => {
         }]
       })
     }
-
+/*
     discovery.query({ environment_id: '057a6f5b-d16b-4465-b163-dfe7e674e8ac', collection_id: '219f9473-11a9-4b78-b68b-9c9aa3e296b3', query: message }, function(err, data) {
       var articles = [];
 
@@ -89,24 +89,9 @@ slapp.command('/playlist', (msg) => {
             }]
           })
         });
-
-/*
-        var articles = data.results;
-        for (var i = 0; i < articles.length; i++) {
-          var title = articles[i].title;
-          var url = articles[i].url;
-
-          msg.respond({
-            text: title,
-            attachments: [{
-              title_url: url;
-            }]
-          });
-        }
-*/
       }
     });
-
+*/
     spotifyApi.searchArtists(message, { limit: 10, offset: 20 }, function(err, data) {
       if (err) {
         console.error('Something went wrong!');
